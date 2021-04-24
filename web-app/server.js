d8e5baf652d9261636421fbd6b72a9adce0625d3
Maximilian Bossert <max.bossert@outlook.de>
2021-04-24 17:43:28 +0200

    Add CORS for React Web App on Port 3000

--- web-app/server.js
+++ web-app/server.js
@@ -1,17 +1,27 @@
 const express = require("express");
-const path = require('path');
-const apiRouter = require('./router');
+const path = require("path");
+const cors = require("cors");
+const apiRouter = require("./router");
 
 const app = express();
 const port = 5000;
 
+// Aktivieren von CORS für die React App auf Port 3000
+// Die React Build Version läuft über Port 5000 und muss nicht über CORS freigegeben werden
+const corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+app.use(cors(corsOptions));
+
+// Ermöglichen von JSON POST requests
 app.use(express.json());
 
 // Bereitstellen der statischen Dateien der gebuildeteten React App (HTML, CSS, JS)
 app.use(express.static(path.join(__dirname, "client/build")));
 
 // Bereitstellen der Router
-app.use('/api', apiRouter);
+app.use("/api", apiRouter);
 
 // Behandelt alle Anfragen, die nicht mit den oben genannten übereinstimmen
 app.get("*", (req, res) => {
