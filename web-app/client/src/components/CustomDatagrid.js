ea67b7c176a6e50b1c8344c0eea0abe7fe348f77
Maximilian Bossert <max.bossert@outlook.de>
2021-04-25 14:30:03 +0200

    Add Data table, Dashboard Container and Dashboard Items

--- web-app/client/src/components/CustomDatagrid.js
+++ web-app/client/src/components/CustomDatagrid.js
@@ -1,35 +1,33 @@
-import React, { useState, useEffect } from "react";
-import DataGrid from "react-data-grid";
+import React from "react";
 
-function ViewContainer(props) {
-  const [darkModeOn, setDarkModeOn] = useState();
-
-  const callback = function (mutationsList, observer) {
-    for (const mutation of mutationsList) {
-      if (mutation.type === "attributes") {
-        document.body.classList.contains("dark-mode") ? setDarkModeOn(true) : setDarkModeOn(false);
-      }
-    }
-  };
-
-  const observer = new MutationObserver(callback);
-
-  // Start observing the target node for configured mutations
-  observer.observe(document.body, {
-    attributes: true,
-    attributeFilter: ["class"],
-    childList: false,
-    characterData: false,
-  });
+function CustomDatagrid(props) {
+  const { columns, rows } = props;
 
   return (
-    <DataGrid
-      columns={props.columns}
-      rows={props.rows}
-      className={darkModeOn ? "bg-dark" : "bg-light"}
-      rowClass={(row) => (darkModeOn ? "bg-dark text-light" : "bg-light text-dark")}
-    />
+    <>
+      {rows && (
+        <table class="table table-hover">
+          <thead>
+            <tr>
+              {columns.map((col) => (
+                <th>{col.name}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {rows.map((row) => (
+              <tr>
+                {columns.map((col) => (
+                  <td>{row[col.key]}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+      {!rows && <div>Keine Daten vorhanden</div>}
+    </>
   );
 }
 
-export default ViewContainer;
+export default CustomDatagrid;
