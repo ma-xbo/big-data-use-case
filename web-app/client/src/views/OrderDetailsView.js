fdf129946f8a306c86f35bca6319d5597b2f38cc
Maximilian Bossert <max.bossert@outlook.de>
2021-05-01 23:33:32 +0200

    Add Map to OrderDetails View

--- web-app/client/src/views/OrderDetailsView.js
+++ web-app/client/src/views/OrderDetailsView.js
@@ -1,25 +1,52 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import ViewContainer from "../components/ViewContainer";
 
 function OrderDetailsView(props) {
   const { order_id } = useParams();
   const [orderDetails, setOrderDetails] = useState();
 
-  console.log(order_id);
-  // fetch orders6
-  useEffect(async () => {
-    const url = "http://localhost:5000/api/order/"+order_id;
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => setOrderDetails(data));
+  useEffect(() => {
+    async function fetchData() {
+      const url = "http://localhost:5000/api/order/" + order_id;
+      await fetch(url)
+        .then((response) => response.json())
+        .then((data) => setOrderDetails(data));
+    }
+    fetchData();
   }, []);
 
   return (
     <ViewContainer title="Details der Bestellung">
-      {orderDetails && <div>{order_id}</div>}
-      {!orderDetails && <p>Keine Daten vorhanden</p>}
+      {orderDetails && (
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-12 col-xl-6">
+              <div>{order_id}</div>
+              <div>{orderDetails.dish_name}</div>
+            </div>
+            <div className="col-12 col-xl-6">
+              <MapContainer
+                center={[orderDetails.store_lat, orderDetails.store_lon]}
+                zoom={13}
+                scrollWheelZoom={false}
+                style={{ height: "300px" }}
+              >
+                <TileLayer
+                  attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[orderDetails.store_lat, orderDetails.store_lon]}>
+                  <Popup>{orderDetails.store_name}</Popup>
+                </Marker>
+              </MapContainer>
+            </div>
+          </div>
+        </div>
+      )}
 
+      {!orderDetails && <p>Keine Daten vorhanden</p>}
     </ViewContainer>
   );
 }
