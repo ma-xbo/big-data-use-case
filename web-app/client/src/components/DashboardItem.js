6b5ab4ec30001fed2adaeb11d1f4755a18981072
Maximilian Bossert <max.bossert@outlook.de>
2021-05-06 21:36:57 +0200

    add shadow to DashboardItem

--- web-app/client/src/components/DashboardItem.js
+++ web-app/client/src/components/DashboardItem.js
@@ -5,7 +5,7 @@ function DashboardItem(props) {
 
   return (
     <div className="col-6 col-xl-3">
-      <div className="card p-15">
+      <div className="card p-15 shadow">
         <div className="text-center font-size-24 font-weight-bold">{value}</div>
         <div className="text-center font-size-16">{description}</div>
       </div>
