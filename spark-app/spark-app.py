0ea33e5ce7f8badb3c077487c5e50456ba0b51b6
Stansuro <Stansuro@users.noreply.github.com>
2021-06-03 14:00:43 +0200

    Added Spark functionality to write raw orders to mysql

--- spark-app/spark-app.py
+++ spark-app/spark-app.py
@@ -3,6 +3,7 @@ from pyspark.sql.functions import *
 from pyspark.sql.types import IntegerType, StringType, StructType, TimestampType
 import mysqlx
 import logging as log
+from datetime import datetime
 
 dbOptions = {"host": "my-app-mysql-service", 'port': 33060, "user": "root", "password": "mysecretpw"}
 dbSchema = 'popular'
@@ -61,6 +62,7 @@ trackingMessages_0 = getMessages()
 trackingMessages_1 = getMessages()
 trackingMessages_2 = getMessages()
 trackingMessages_3 = getMessages()
+trackingMessages_4 = getMessages()
 
 # Example Part 4
 # Compute most popular dishes by count
@@ -222,6 +224,25 @@ def saveToDatabase_store_revenue(batchDataframe, batchId):
     # Perform batch UPSERTS per data partition
     batchDataframe.foreachPartition(save_to_db_store_revenue)
 
+def saveToDatabase_rawOrders(batchDataframe, batchId):
+    # Define function to save a dataframe to mysql
+    def save_to_db_rawOrders(iterator):
+        # Connect to database and use schema
+        session = mysqlx.get_session(dbOptions)
+        session.sql("USE popular").execute()
+
+        for row in iterator:
+            # Run upsert (insert or update existing)
+            sql = session.sql("INSERT IGNORE INTO orders "
+                              "(order_id, dish_id, store_id, timestamp) VALUES (?, ?, ?, ?)")
+            parsed_timestamp = datetime.utcfromtimestamp(row.timestamp).strftime('%Y-%m-%d %H:%M:%S')
+            sql.bind(row.order_id, row.dish_id, row.store_id, parsed_timestamp).execute()
+
+        session.close()
+
+    # Perform batch UPSERTS per data partition
+    batchDataframe.foreachPartition(save_to_db_rawOrders)
+
 # Example Part 7
 dbInsertStream = dish_count.writeStream \
     .trigger(processingTime=slidingDuration) \
@@ -245,6 +266,12 @@ dbInsertStream = store_revenue.writeStream \
     .trigger(processingTime=slidingDuration) \
     .outputMode("update") \
     .foreachBatch(saveToDatabase_store_revenue) \
+    .start()
+
+dbInsertStream = trackingMessages_4.writeStream \
+    .trigger(processingTime=slidingDuration) \
+    .outputMode("update") \
+    .foreachBatch(saveToDatabase_rawOrders) \
     .start()
 
 # Wait for termination
